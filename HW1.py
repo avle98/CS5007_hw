15da2064c4ccb1826241866d8cd44fac4f4cee08
Ann <avle9721@gmail.com>
2020-05-27 16:54:10 -0400

    init

---
+++ HW1.py
@@ -0,0 +1,197 @@
+#   HW1 Assignment 
+#   Author: <INSERT FIRST NAME> <INSERT LAST NAME> 
+#   Save this file as FIRSTNAME-LASTNAME-HW1.py
+
+
+
+#   -------
+#   Part 1:
+#   -------
+
+print("Part 1")
+
+#   Let the following variables be defined with the following
+#   values
+
+a = 5.0
+b = 2.5
+c = 9.0
+
+#   1a: (write expression in place of the zero in the next line)
+expr_1a = 0
+print("The value of expression 1a is", expr_1a)
+
+#   1b: (write expression in place of the zero in the next line)
+expr_1b = 0
+print("The value of expression 1b is", expr_1b)
+
+#   1c: (write expression in place of the zero in the next line)
+expr_1c = 0
+print("The value of expression 1c is", expr_1c)
+
+#   1d: (write expression in place of the zero in the next line)
+expr_1d = 0
+print("The value of expression 1d is", expr_1d)
+
+#   1e: (write expression in place of the zero in the next line)
+expr_1e = 0
+print("The value of expression 1e is", expr_1e, "\n")
+
+#   -------
+#   Part 2:
+#   -------
+
+print("Part 2")
+
+#   2a: (write expression in place of the zero in the next line)
+expr_2a = (8//6)*6 
+print("The value of expression 2a is", expr_2a)
+
+#   2b: (write expression in place of the zero in the next line)
+expr_2b = (8/6)*6
+print("The value of expression 2b is", expr_2b)
+
+#   Write here your comment (max. two sentences)
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+
+
+#   -------
+#   Part 3:
+#   -------
+
+print("\nPart 3")
+
+#   Let the following variables be defined with the following
+#   values
+
+a = True
+b = False
+c = True
+
+#   3a: (write expression in place of the True in the next line)
+expr_3a = True
+print("The value of expression 3a is", expr_3a)
+
+#   3b: (write expression in place of the True in the next line)
+expr_3b = True
+print("The value of expression 3b is", expr_3b)
+
+#   3c: (write expression in place of the True in the next line)
+expr_3c = True
+print("The value of expression 3c is", expr_3c)
+
+#   3d: (write expression in place of the True in the next line)
+expr_3d = True
+print("The value of expression 3d is", expr_3d)
+
+#   -------
+#   Part 4:
+#   -------
+
+print("\nPart 4")
+
+#   1) Write here your windChill function.
+
+
+
+
+
+
+
+
+
+#   Call here your windChill function with arguments -20 for T and 30 for V. 
+
+
+
+
+
+#   2) Write here your windChill2 function.
+
+
+
+
+
+
+
+#   Call here your windChill2 function with arguments -20 for T and 30 for V.
+#   Print the result of this call. 
+
+
+
+
+
+
+
+#   -------
+#   Part 5:
+#   -------
+
+print("\nPart 5")
+
+#   List here the 5 errors in the Python function euc
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+#   Write correcly the function, the call of this function and
+#   the print statement:
+
+
+
+
+
+
+
+
+
+
+
+
+
+#   ------------
+#   Extra credit
+#   ------------
+
+print("\nExtra credit")
+
+def myFct(value):
+    if(value==1):
+        return value
+    else:
+        return value+myFct(value-1)
+
+
+#   Write here your comment:
+#
+#
+#
+#
+
+#   Write here a simpler function, doing the same without
+#   if/else statements and without re-calling myFct
+
+def myFct(value):
+    # Write here the code of the function in place of the print('TODO')
+    print('TODO')
+
+
+
+# END
