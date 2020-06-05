7f9da1939866fe4fdf12dc485a7e757ee9d631f2
Ann <avle9721@gmail.com>
2020-06-05 19:28:18 -0400

    init

---
+++ ANN-ELIZABETH-LE-HW2.py
@@ -0,0 +1,83 @@
+#   HW2 Assignment 
+#   Author: Ann-Elizabeth Le
+
+#   -------
+#   Part 1: Patterns
+#   -------
+
+#   1) Write a function that given two integers, prints out two identical recentagles
+#   of width n and height m, separated by an empty column with '*'
+def rectangle(n, m):
+    pass
+
+
+#   2) Write a function that given a positive integer n, prints out a triangle of height n with '*'
+#   The triangle should follow the rules:
+#   (a) Each row at depth i, i >0 has two more stars than the number of stars of the previous row at depth i-1 
+#   (b) At depth 0, the function prints 1 star
+def triangle(n):
+    pass
+
+
+#   -------
+#   Part 2: Function on lists
+#   -------
+
+#   Write a function that takes a list of integers aList as argument
+#   and returns a new list where multiple occurrences of values have been removed.
+def clean1(aList):
+    pass
+
+
+
+#   -------
+#   Part 3: Function on lists 
+#   -------
+
+#   Write a function that takes a list of integers aList as argument and modifies this list
+#   and does not return a list.
+def clean2(aList):
+    pass
+
+
+
+
+#   -------
+#   Part 4: Function on lists
+#   -------
+
+#   Write a function that takes a list of integers aList as arguments
+#   and returns a string containing elements of the list that are not a power of 2, all on the same line
+#   Write a call to this function with arguments embedded in a print statement
+def fct(aList):
+    pass
+
+
+
+#   -------
+#   Part 5: Taylor Sinus 
+#   -------
+
+#   Write a function that approximates the sin(x) by
+#   returning the sum of the first n elements of its Taylor series
+def taylor(x,n):
+    pass
+
+
+
+
+
+
+#   ------------
+#   Extra credit
+#   ------------
+
+#   Implement a function that takes a list of integers as argument
+#   and return the max number of consecutive integers that are all equal witin this list
+def consecutives(aList):
+    pass
+
+
+
+
+# END
