dd10e0af0e64913de3bd8efa247072243a9e3f7a
Ann <avle9721@gmail.com>
2020-06-07 14:25:33 -0400

    complete everything but problem 3

--- ANN-ELIZABETH-LE-HW2.py
+++ ANN-ELIZABETH-LE-HW2.py
@@ -1,6 +1,8 @@
 #   HW2 Assignment 
 #   Author: Ann-Elizabeth Le
 
+import math
+
 #   -------
 #   Part 1: Patterns
 #   -------
@@ -11,12 +13,12 @@ def rectangle(n, m):
     symbol = '*'
     empty_col = " "
     for i in range(n):
-        rect = n*symbol
-        print (rect + empty_col + rect)
+        rect = n * symbol
+        print(rect + empty_col + rect)
 
-#calling the function with an example
-print ("Example output for Part 1.1: ")
-rectangle(4,6)
+# calling the function with an example
+print("Example output for Part 1.1: ")
+rectangle(4, 6)
 
 
 #   2) Write a function that given a positive integer n, prints out a triangle of height n with '*'
@@ -24,17 +26,17 @@ rectangle(4,6)
 #   (a) Each row at depth i, i >0 has two more stars than the number of stars of the previous row at depth i-1 
 #   (b) At depth 0, the function prints 1 star
 def triangle(n):
-    k = 2*n-2
+    k = 2 * n - 2
     for i in range(0, n):
         for j in range(0, k):
             print(end=" ")
-        k = k-1
-        for j in range(0, i+1):
+        k = k - 1
+        for j in range(0, i + 1):
             print("* ", end="")
         print("\r")
 
-#calling the function with an example
-print ('\n' + "Example output for Part 1.2: ")
+# calling the function with an example
+print('\n' + "Example output for Part 1.2: ")
 triangle(6)
 
 
@@ -51,9 +53,10 @@ def clean1(aList):
             tmp.append(i)
     return tmp
 
-#calling the function with an example
-print ('\n' + "Example output for Part 2: ")
-al = [1,2,3,4,4,4,5,1,2,1,5]
+
+# calling the function with an example
+print('\n' + "Example output for Part 2: ")
+al = [1, 2, 3, 4, 4, 4, 5, 1, 2, 1, 5]
 newlist = clean1(al)
 print(al)
 print(newlist)
@@ -66,16 +69,15 @@ print(newlist)
 #   Write a function that takes a list of integers aList as an argument,
 #   modifies this list and does not return a list.
 def clean2(aList):
-    return list(set(aList))
+    aList = list(set(aList))
 
 
-#calling the function with an example
+# calling the function with an example
 print('\n' + "Example output for Part 3: ")
-al = [1,2,3,4,4,4,5,1,2,1,5]
+al = [1, 2, 3, 4, 4, 4, 5, 1, 2, 1, 5]
 print(al)
 clean2(al)
 print(al)
-
 
 #   -------
 #   Part 4: Function on lists
@@ -85,12 +87,18 @@ print(al)
 #   and returns a string containing elements of the list that are not a power of 2, all on the same line
 #   Write a call to this function with arguments embedded in a print statement
 def fct(aList):
-    pass
+    tmp = []
+    for i in aList:
+        if i & (i-1) != 0:
+            tmp.append(i)
+    return tmp
 
-#calling the function with an example
-print ('\n' + "Example output for Part 4: ")
-al =  [1, 2, 3, 4, 5, 16, 255, 256, −1, −2, −4, 84, 8, 16, 4, 6]
-print (fct(al))
+# calling the function with an example
+print('\n' + "Example output for Part 4: ")
+al = [1, 2, 3, 4, 5, 16, 255, 256, -1, -2, -4, 84, 8, 16, 4, 6]
+print(al)
+print(fct(al))
+
 
 #   -------
 #   Part 5: Taylor Sinus 
@@ -98,11 +106,17 @@ print (fct(al))
 
 #   Write a function that approximates the sin(x) by
 #   returning the sum of the first n elements of its Taylor series
-def taylor(x,n):
-    pass
+def taylor(x, n):
+    approx = 0
+    for i in range(n):
+        approx += (math.pow(-1,i))/(math.factorial(2*i+1))*(math.pow(x,2*i+1))
+    return approx
 
-#calling the function with an example
-print ('\n' + ''"Example output for Part 5: ")
+# calling the function with an example
+print('\n' + "Example output for Part 5: ")
+x = math.pi/2
+print(math.sin(x))
+print(taylor(x,7))
 
 
 #   ------------
@@ -112,8 +126,30 @@ print ('\n' + ''"Example output for Part 5: ")
 #   Implement a function that takes a list of integers as argument
 #   and return the max number of consecutive integers that are all equal witin this list
 def consecutives(aList):
-    pass
+    i = 0
+    consec_int = 0   
+    while 0 <= i < len(aList):
+        counter = 1  
+        for j in range(i+1, len(aList)):
+            if aList[j] != aList[i]:
+                i = j
+                break
+            counter += 1
+            i += 1    
+        if counter > consec_int:
+            consec_int = counter
+        if i == len(aList) - 1:
+            break
+    return consec_int 
 
+# calling the function with an example
+print('\n' + "Example output for extra credit: ")
+l1 = [1,1,1,3,3,4,4,4,4,4,3,1,7,3,3,4,4]
+l2 = []
+l3 = [1,2,1,1,2,1,1]
+print(consecutives(l1))
+print(consecutives(l2))
+print(consecutives(l3))
 
 
 
