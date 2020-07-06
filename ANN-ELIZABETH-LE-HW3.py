114b26abf1d112ba1b72ce2e190b61c7174b3032
Ann <avle9721@gmail.com>
2020-07-06 16:36:50 -0400

    final commit

--- ANN-ELIZABETH-LE-HW3.py
+++ ANN-ELIZABETH-LE-HW3.py
@@ -25,22 +25,25 @@ class Point:
 
     # returns coordinates with specific format
     def toString(self):
-        return "({}, {})".format(self.x, self.y)
+        #return "({}, {})".format(self.x, self.y)
+        return '(' + str(self.x) + ', ' + str(self.y) + ')'
+
 
     # compares 2 Points to see if they are similar or different
     def equals(self, point):
-        if self.x == point.x and self.y == point.y:
-            return "True"
-        else:
-            return "False"
+        # if self.x == point.x and self.y == point.y:
+        #     return "True"
+        # else:
+        #     return "False"
+        return self.x == point.getX() and self.y == point.getY()
 
 
 print("PART 1: POINT")
 # create points p1 and p2
-p1 = Point(0, 0)
+p1 = Point()
 p2 = Point(1, 2)
-print("p1 and p2 --> " + p1.toString() + " " + p2.toString())  # print out coordinates of 2 points
-print("p1 equals p2? --> " + p1.equals(p2))  # print out result of applying equals method of p1 to p2
+print("p1 and p2 --> ", p1.toString(), " ", p2.toString())  # print out coordinates of 2 points
+print("p1 equals p2? --> ", p1.equals(p2))  # print out result of applying equals method of p1 to p2
 
 # set x coordinate of p2 equal to x coordinate of p1
 p1X = p1.getX()
@@ -48,14 +51,13 @@ p2.setX(p1X)
 # set y coordinate of p2 equal to y coordinate of p1
 p1Y = p1.getY()
 p2.setY(p1Y)
-print("\nset p2's coordinates to p1's --> " + p2.toString())  # print out result of applying equals method of p1 to p2
-print("p1 equals p2? --> " + p1.equals(p2))  # print out result of applying equals method of p1 to p2
+print("\nset p2's coordinates to p1's --> ", p2.toString())  # print out result of applying equals method of p1 to p2
+print("p1 equals p2? --> ", p1.equals(p2))  # print out result of applying equals method of p1 to p2
 
 # increment 1 to x coordinate of p2
-p2plus = p2.getX()
-p2.setX(p2plus + 1)
-print("\np2's x coordinate --> " + p2.toString())  # print out new coordinates of p2
-print("p1 equals p2? --> " + p1.equals(p2))  # print out result of applying equals method of p1 to p2
+p2.setX(p2.getX() + 1)
+print("\np2's x coordinate --> ", p2.toString())  # print out new coordinates of p2
+print("p1 equals p2? --> ", p1.equals(p2))  # print out result of applying equals method of p1 to p2
 
 
 #   -------
@@ -65,7 +67,7 @@ print("p1 equals p2? --> " + p1.equals(p2))  # print out result of applying equa
 #   State a class Rectangle that has 3 instance variables: a point, a rational width, and a rational height
 class Rectangle:
     def __init__(self, point, w, h):
-        self.point = point
+        self.bottomLeft = point
         self.width = w
         self.height = h
 
@@ -81,18 +83,20 @@ class Rectangle:
 
     # returns reference to point representing left bottom corner of rectangle
     def getOrigin(self):
-        o = self.point.x, self.point.y
+        #o = self.point.x, self.point.y
+        o = self.bottomLeft
         return o
 
     # returns string with specific format
     def toString(self):
-        s = "({}, {}), L={}, H={}".format(self.point.x, self.point.y, self.width, self.height)
+        #s = "({}, {}), L={}, H={}".format(self.point.x, self.point.y, self.width, self.height)
+        s = self.getOrigin().toString() + ", W=" + str(self.width) + ", H=" + str(self.height)
         return s
 
 
 print("\n\nPART 2: RECTANGLE")
 # create rectangles r1 and r2
-p1 = Point(0,0)
+p1 = Point()
 r1 = Rectangle(p1, 2, 5.5)
 p2 = Point(1, 2)
 r2 = Rectangle(p2, 3, 6)
@@ -117,8 +121,8 @@ print("r2 toString --> ", r2.toString())
 #   State a class MovableRectangle that inherits from the class Rectangle
 #   This class has 3 arguments: a point and 2 rational values to be assigned to 3 instance variables
 class MovableRectangle(Rectangle):
-    def __init__(self, point, w, h):
-        super().__init__(point, w, h)
+    def __init__(self, bottomLeft, w, h):
+        Rectangle.__init__(self, bottomLeft, w, h)
         self.move = False
 
     # sets self.move to true
@@ -132,31 +136,34 @@ class MovableRectangle(Rectangle):
     # detects whether rectangle is locked or unlocked
     def moveTo(self, newPoint):
         if self.move is True:  # if rectangle is unlocked
-            self.point = newPoint  # moves rectangle so new origin is the point argument
+            #self.bottomLeft = newPoint  # moves rectangle so new origin is the point argument
+            self.getOrigin().setX(newPoint.getX())
+            self.getOrigin().setY(newPoint.getY())
         else:
             print("Warning: locked.")
 
     # returns string with specific format
     def toString(self):
-        s = "({}, {}), W={}, H={}, Movable? {}".format(self.point.x, self.point.y, self.width, self.height, self.move)
+        #s = "({}, {}), W={}, H={}, Movable? {}".format(self.point.x, self.point.y, self.width, self.height, self.move)
+        s = Rectangle.toString(self) + ", Movable? " + str(self.move)
         return s
 
 
 print("\n\nPART 3: MOVABLE RECTANGLE")
 # create movable rectangle r3
-p1 = Point(0,0)
+p1 = Point()
 r3 = MovableRectangle(p1, 2, 2)
-print("r3 toString --> " + r3.toString())  # calls toString method
+print("r3 toString --> ", r3.toString())  # calls toString method
 
 p3 = Point(5, 15)
 print("\nCan it be moved?")
 r3.moveTo(p3)  #calls moveTo method on r3 to move origin to p3
-print("r3 with moveTo method on p3 --> " + r3.toString())  # calls toString method
+print("r3 with moveTo method on p3 --> ", r3.toString())  # calls toString method
 
 r3.unlock()
 print("\nCan it be moved?")
 r3.moveTo(p3)  #calls moveTo method on r3 to move origin to p3
-print("r3 with moveTo method on p3 --> " + r3.toString())  # calls toString method
+print("r3 with moveTo method on p3 --> ", r3.toString())  # calls toString method
 
 
 #   ------------
@@ -166,19 +173,29 @@ print("r3 with moveTo method on p3 --> " + r3.toString())  # calls toString meth
 #   Create a class Tour with a single instance variable self.MyList that will refer to a list object
 class Tour:
     def __init__(self, filename):
-        file = open(filename, 'r')
         self.myList = []
+        file = open(filename, 'r')
         for line in file.readlines():
             row = []
             for value in line.split(","):
                 row.append(value.strip())
                 self.myList.append(row)
+        file.close()
 
-if __name__ == '__main__':
-    print("\n\nPART 4: EXTRA CREDIT - READ FILE")
-    filename = input('Enter input filename: ')
-    t = Tour(filename)
-    print(t.myList)
+        # alternative method
+        # self.myList = []
+        # file = open(filename)
+        # for line in file:
+        #     l = line.split(',')
+        #     for i in range(0, len(l)):
+        #         l[i] = l[i].rstrip('\n')
+        #     self.myList.append(l)
+        # file.close()
+
+print("PART 4: EXTRA CREDIT - READ FILE")
+filename = input('Enter input filename: ')
+t = Tour(filename)
+print(t.myList)
 
 
 #   END
